5648171bbb1f38f5c00385278278b3d9038bafe7
TDpy <toto.dupuy6558@gmail.com>
2023-03-01 11:57:19 +0100

    create api to get maps, can be filtered by level (= difficulty)

---
+++ sokoban_api/README.md
@@ -0,0 +1,9 @@
+pour lancer l'API :
+```
+cd ./sokoban_api
+symfony server:start
+```
+
+Configuration api :
+create DB sokoban-react_native
+
---
+++ sokoban_api/src/Entity/Map.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\MapRepository;
+use Doctrine\ORM\Mapping as ORM;
+// use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+
+
+#[ORM\Entity(repositoryClass: MapRepository::class)]
+#[ApiResource(
+    operations: [
+        // new Get(),
+        new GetCollection(),
+    ],
+)]
+#[ApiFilter(SearchFilter::class, properties: ['level' => 'exact'])]
+
+class Map
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $level = null;
+
+    #[ORM\Column]
+    private array $matrix = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getMatrix(): array
+    {
+        return $this->matrix;
+    }
+
+    public function setMatrix(array $matrix): self
+    {
+        $this->matrix = $matrix;
+
+        return $this;
+    }
+}
