53e0862234a1306db8607d1e4587c75577903d3d
Grégoire <nodilod@gmail.com>
2023-03-22 11:35:13 +0100

    move player

--- ReactNative-sokoban/App.js
+++ ReactNative-sokoban/App.js
@@ -1,39 +1,80 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import Header from './src/Composants/Header/Header';
 import Grid from './src/Composants/Map/Grid';
 import ArrowButtons from './src/Composants/ArrowsButton/ArrowsButton';
 
 const App = () => {
-  const convertGrid = (grid) => {
-    return grid.map(row => {
-      return row.split('');
-    });
-  };
-  
-  const initialGrid = convertGrid([
-    "..####...",
-    "###..####",
-    "#.....C.#",
-    "#.#..#C.#",
-    "#.X.X#P.#",
-    "#########"
-  ]);
-  
-  const [grid, setGrid] = useState(initialGrid);
+    const convertGrid = (grid) => {
+        return grid.map(row => {
+            return row.split('').reverse();
+        });
+    };
 
-  const handleMove = (direction) => {
-   
-  };
+    const initialGrid = convertGrid([
+        "..#.##...",
+        "###..####",
+        "#.....C.#",
+        "..#..#C..",
+        "#.X.X#P.#",
+        "####....#"
+    ]);
+
+    const [grid, setGrid] = useState(initialGrid);
+
+    const handleMove = (direction) => {
+        let newGrid = [...grid];
+        let playerPosition = [];
+        let playerState = '';
+        grid.forEach((row, rowIndex) => {
+            row.forEach((cell, cellIndex) => {
+                if (cell === 'P' || cell === 'Q') {
+                    playerPosition = [rowIndex, cellIndex]
+                    playerState = cell;
+                    return;
+                }
+            });
+        })
+
+        let newPositions = [];
+        switch (direction) {
+            case 'left':
+                newPositions = [playerPosition[0] - 1, playerPosition[1]];
+                break;
+            case 'right':
+                newPositions = [playerPosition[0] + 1, playerPosition[1]];
+                break;
+            case 'up':
+                newPositions = [playerPosition[0], playerPosition[1] - 1];
+                break;
+            case 'down':
+                newPositions = [playerPosition[0], playerPosition[1] + 1];
+                break;
+            default:
+                break;
+        }
+
+        if( newPositions[0] < 0 || newPositions[1] < 0 || newGrid[newPositions[0]][newPositions[1]] === '#') {
+            return;
+        } else {
+            newGrid[newPositions[0]][newPositions[1]] = newGrid[newPositions[0]][newPositions[1]] === 'C' ? 'Q' : 'P';
+            newGrid[playerPosition[0]][playerPosition[1]] = playerState === 'P' ? '.' : 'C';
+            setGrid(newGrid);
+        }
+    };
+
+    const moveElement = (grid, direction) => {
+
+    }
 
 
-  return (
-    <View>
-      <Header />
-      <Grid grid={grid} />
-      <ArrowButtons handleMove={handleMove} />
-    </View>
-  );
+    return (
+        <View>
+            <Header/>
+            <Grid grid={grid}/>
+            <ArrowButtons handleMove={handleMove}/>
+        </View>
+    );
 };
 
 export default App;
