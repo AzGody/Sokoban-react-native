558631210a15e16cf292f9916a840b198a06de2d
AzGody <obastien4@gmail.com>
2023-03-22 09:20:34 +0100

    Init map + direction + header

--- ReactNative-sokoban/App.js
+++ ReactNative-sokoban/App.js
@@ -1,23 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Composants/Header/Header.js'
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 
-export default function App() {
+const Header = () => {
   return (
-    <View style={styles.container}>
-      <Header/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginTop: 20 }}>Sokoban</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Grid = ({ grid }) => {
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 20 }}>
+      {grid.map((row, rowIndex) => (
+        <View key={rowIndex} style={{ flexDirection: 'column' }}>
+          {row.map((cell, cellIndex) => (
+            <Image key={cellIndex} source={{
+              uri: 'https://www.bebegavroche.com/media/catalog/product/cache/1/thumbnail/1200x/040ec09b1e35df139433887a97daa66f/s/c/sc917-figurine-en-carton-shrek-debout-qui-sourit--94--cm-1.jpg',
+            }} style={{ width: 50, height: 50 }} />
+          ))}
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const App = () => {
+  const convertGrid = (grid) => {
+    return grid.map(row => {
+      return row.split('');
+    });
+  };
+  
+  const initialGrid = convertGrid([
+    "..####...",
+    "###..####",
+    "#.....C.#",
+    "#.#..#C.#",
+    "#.X.X#P.#",
+    "#########"
+  ]);
+  
+  const [grid, setGrid] = useState(initialGrid);
+
+  const handleMove = (direction) => {
+    /* Your move logic here */
+  };
+
+  return (
+    <View>
+      <Header />
+      <Grid grid={grid} />
+      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableWithoutFeedback onPress={() => handleMove('up')}>
+          <Image source={{
+          uri: 'https://www.bebegavroche.com/media/catalog/product/cache/1/thumbnail/1200x/040ec09b1e35df139433887a97daa66f/s/c/sc917-figurine-en-carton-shrek-debout-qui-sourit--94--cm-1.jpg',
+        }} style={{ width: 50, height: 50 }} />
+        </TouchableWithoutFeedback>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TouchableWithoutFeedback onPress={() => handleMove('left')}>
+          <Image source={{
+          uri: 'https://www.bebegavroche.com/media/catalog/product/cache/1/thumbnail/1200x/040ec09b1e35df139433887a97daa66f/s/c/sc917-figurine-en-carton-shrek-debout-qui-sourit--94--cm-1.jpg',
+        }} style={{ width: 50, height: 50 }} />
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => handleMove('right')}>
+          <Image source={{
+          uri: 'https://www.bebegavroche.com/media/catalog/product/cache/1/thumbnail/1200x/040ec09b1e35df139433887a97daa66f/s/c/sc917-figurine-en-carton-shrek-debout-qui-sourit--94--cm-1.jpg',
+        }} style={{ width: 50, height: 50 }} />
+        </TouchableWithoutFeedback>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableWithoutFeedback onPress={() => handleMove('down')}>
+          <Image source={{
+          uri: 'https://www.bebegavroche.com/media/catalog/product/cache/1/thumbnail/1200x/040ec09b1e35df139433887a97daa66f/s/c/sc917-figurine-en-carton-shrek-debout-qui-sourit--94--cm-1.jpg',
+        }} style={{ width: 50, height: 50 }} />
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  );
+};
+
+export default App;
